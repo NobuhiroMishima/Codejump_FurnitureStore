73a16c6fed292c8ff9879dc0bf3d4abe55aba881
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-06-25 21:45:52 +0900

    update footer

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,36 @@ const menuOpen = function(){
 
 btn.addEventListener('click', menuOpen);
 cover.addEventListener('click', menuOpen);
+
+function footerFixed(){
+    //ドキュメントの高さ
+    var dh = document.getElementsByTagName("body")[0].clientHeight;
+    //フッターのtopからの位置
+    document.querySelector('.footer').style.top = "0px";
+    var ft = document.querySelector('.footer').offsetTop;
+    //フッターの高さ
+    var fh = document.querySelector('.footer').offsetHeight;
+
+    //ウィンドウの高さ
+    if (window.innerHeight){
+        var wh = window.innerHeight;
+    }else if(document.documentElement && document.documentElement.clientHeight != 0){
+        var wh = document.documentElement.clientHeight;
+    }
+    if(ft+fh<wh){
+        document.querySelector('.footer').style.position = "relative";
+        document.querySelector('.footer').style.top = (wh-fh-ft-1)+"px";
+    }
+}
+
+//イベントリスナー
+function addEvent(elm,listener,fn){
+    try{
+        elm.addEventListener(listener,fn,false);
+    }catch(e){
+        elm.attachEvent("on"+listener,fn);
+    }
+}
+
+addEvent(window,"load",footerFixed);
+addEvent(window,"resize",footerFixed);
