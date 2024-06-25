49f10cdc3f67a606a87abbcffc1f55f304c9c580
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-06-25 21:05:26 +0900

    update menu-cover

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('.menu__btn');
 const cover = document.querySelector('.menu__cover');
 
 const menuOpen = function(){
-    cover.classList.toggle('menu__open')
+    cover.classList.toggle('menu__open');
 }
 
 btn.addEventListener('click', menuOpen);
