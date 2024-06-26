c9b2d83afdc7075d2c02f657227cbcadd5911487
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-06-26 19:53:18 +0900

    add header/footer php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <ul class="sns">
+      <li><a class="hover" href="#">INSTAGRAM</a></li>
+      <li><a class="hover" href="#">TWITTER</a></li>
+      <li><a class="hover" href="#">FACEBOOK</a></li>
+    </ul>
+    <p class="copy">&copy; Funiture Design</p>
+  </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/vendors/css-reset.css" />
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="scripts/main.js" defer></script>
+</head>
+<body>
+  <header class="header menu__cover">
+    <h1><a href="/index.html"><img src="images/logo.svg"></a></h1>
+    <nav class="header__nav">
+      <ul class="header__ul">
+        <li class="header__li item"><a href="#">PRODUCTS</a></li>
+        <li class="header__li item"><a href="#">ABOUT</a></li>
+        <li class="header__li item"><a href="#">COMPANY</a></li>
+        <li class="header__li item"><a href="#">CONTACT</a></li>
+      </ul>
+    </nav>
+    <button class="menu__btn">
+      <span></span>
+      <span></span>
+    </button>
+    <div class="cover"></div>
+  </header>
